f8871e90753777febabbeaa8b8cc3d390aa7d258
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2019-04-17 17:46:37 +0200

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bioimagepy',
+      version='0.1',
+      description='Manipulation of bio-image data and metadata',
+      url='https://gitlab.inria.fr/serpico/bioimagepy',
+      author='Sylvain Prigent',
+      author_email='sylvain.prigent@inria.fr',
+      license='None',
+      packages=['bioimagepy'],
+      zip_safe=False)
