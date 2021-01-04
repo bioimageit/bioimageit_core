13b8f945949872ba91c5af90c04b0ff3a28b31f0
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-01-04 16:32:38 +0100

    correct error in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'BioImagePy'
+project = 'BioImageIT_core'
 copyright = '2020, Sylvain Prigent'
 author = 'Sylvain Prigent'
 
